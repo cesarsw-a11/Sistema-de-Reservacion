82d309ab41c189245129b48cf6a397358c8692d2
Cesar <menesescesar@hotmail.es>
2021-11-22 20:48:33 -0600

    Primer commit

---
+++ mysql/conexion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace El_Balcon_de_Chalita.mysql
+{
+    class conexion
+    {
+        public static MySqlConnection Conexion()
+        {
+
+            string servidor = "localhost";
+            string basededatos = "elbalcondechalita";
+            string usuario = "root";
+            string password = "";
+
+            string cadenaConexion = "Database=" + basededatos + "; Data Source=" + servidor + "; User Id= " + usuario + "; Password=" + password + "";
+
+
+            try
+            {
+                MySqlConnection conexionBD = new MySqlConnection(cadenaConexion);
+
+                return conexionBD;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return null;
+            }
+        }
+
+    }
+}
